454c67b29bd5a4cd7e33a27132288bda93602cac
sparrowCodes <deepaksu.047@gmail.com>
2024-06-13 23:52:34 +0530

    color generator half push

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import SingleColor from "./SingleColor";
 
 // import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h4>color generator</h4>
+        <form>
+          <input type="color" />
+          <input type="text" placeholder="#f15025" />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors-container">
+        <article className="color false">
+          <p className="percent-value">100%</p>
+          <p className="color-value">#ffffff</p>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
