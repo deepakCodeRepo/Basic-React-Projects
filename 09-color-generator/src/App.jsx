9202c9a02c6618f6b0a335a0b380e5960da1d6d7
sparrowCodes <deepaksu.047@gmail.com>
2024-06-14 22:25:56 +0530

    color generator changes push

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -1,24 +1,45 @@
 import { useState } from "react";
 import SingleColor from "./SingleColor";
 
-// import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("#f15025");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
     <main>
       <section className="container">
         <h4>color generator</h4>
-        <form>
-          <input type="color" />
-          <input type="text" placeholder="#f15025" />
+        <form onSubmit={handleSubmit}>
+          <input type="color" onChange={(e) => setColor(e.target.value)} />
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => setColor(e.target.value)}
+            className={error ? "error" : null}
+          />
           <button type="submit">Submit</button>
         </form>
       </section>
       <section className="colors-container">
-        <article className="color false">
-          <p className="percent-value">100%</p>
-          <p className="color-value">#ffffff</p>
-        </article>
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </main>
   );
--- 09-color-generator/src/SingleColor.jsx
+++ 09-color-generator/src/SingleColor.jsx
@@ -1,8 +1,22 @@
 import { useState, useEffect } from "react";
 import rgbToHex from "./utils";
+import PropTypes from "prop-types";
 
-const SingleColor = () => {
-  return <h4>single color</h4>;
+const SingleColor = ({ rgb, weight, index }) => {
+  const colorValue = rgbToHex(rgb[0], rgb[1], rgb[2]);
+
+  return (
+    <article className="color false">
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{colorValue}</p>
+    </article>
+  );
+};
+
+SingleColor.propTypes = {
+  rgb: PropTypes.array.isRequired,
+  weight: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default SingleColor;
