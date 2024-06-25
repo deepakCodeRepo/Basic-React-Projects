050cc560ce8efe07b0669a20bdca65cd68fd9b0c
sparrowCodes <deepaksu.047@gmail.com>
2024-06-26 02:56:46 +0530

    navbar changes push

--- 11-navbar/src/Navbar.jsx
+++ 11-navbar/src/Navbar.jsx
@@ -1,10 +1,64 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import { useEffect, useRef, useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./assets/react.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [toggleLinks, setToggleLinks] = useState(false);
 
-export default Navbar
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    console.log(linksRef.current.getBoundingClientRect());
+    console.log(linksHeight);
+    if (toggleLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [toggleLinks]);
+
+  return (
+    <main>
+      <nav>
+        <div className="header-container">
+          <div className="nav-header">
+            <img src={logo} alt="logo" />
+            <button
+              className="nav-toggle"
+              onClick={() => setToggleLinks(!toggleLinks)}
+            >
+              <FaBars />
+            </button>
+          </div>
+          <div className="links-container" ref={linksContainerRef}>
+            <ul className="links " ref={linksRef}>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <ul className="social-icons">
+            {social.map((item) => {
+              const { id, url, icon } = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </main>
+  );
+};
+
+export default Navbar;
