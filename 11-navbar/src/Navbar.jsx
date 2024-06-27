41e07eac78a25f80a12b3bff0f8bea3aa2809e00
sparrowCodes <deepaksu.047@gmail.com>
2024-06-27 14:49:52 +0530

    navbar push

--- 11-navbar/src/Navbar.jsx
+++ 11-navbar/src/Navbar.jsx
@@ -1,22 +1,18 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./assets/react.svg";
 
+//INFO: code I came up with
 const Navbar = () => {
-  const [toggleLinks, setToggleLinks] = useState(false);
+  const [toggleLinks, setToggleLinks] = useState(true);
 
-  const linksContainerRef = useRef(null);
-  const linksRef = useRef(null);
-
-  const toggle = () => {
-    const linksHeight = linksRef.current.getBoundingClientRect().height;
-    if (toggleLinks) {
-      linksContainerRef.current.style.height = `${linksHeight}px`;
-    } else {
-      linksContainerRef.current.style.height = "0px";
+  window.onresize = function () {
+    if (window.innerWidth > 800) {
+      setToggleLinks(true);
+    } else if (window.innerWidth <= 800) {
+      setToggleLinks(false);
     }
-    setToggleLinks(!toggleLinks);
   };
 
   return (
@@ -25,7 +21,131 @@ const Navbar = () => {
         <div className="header-container">
           <div className="nav-header">
             <img src={logo} alt="logo" />
-            <button className="nav-toggle" onClick={toggle}>
+            <button
+              className="nav-toggle"
+              onClick={() => setToggleLinks(!toggleLinks)}
+            >
+              <FaBars />
+            </button>
+          </div>
+          {toggleLinks && (
+            <div className="links-container">
+              <ul className="links">
+                {links.map((link) => {
+                  const { id, url, text } = link;
+                  return (
+                    <li key={id}>
+                      <a href={url}>{text}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+          <ul className="social-icons">
+            {social.map((item) => {
+              const { id, url, icon } = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </main>
+  );
+};
+
+export default Navbar;
+
+//INFO: for smooth transition of the links
+/* const Navbar = () => {
+  const [toggleLinks, setToggleLinks] = useState(false);
+
+  return (
+    <main>
+      <nav>
+        <div className="header-container">
+          <div className="nav-header">
+            <img src={logo} alt="logo" />
+            <button
+              className="nav-toggle"
+              onClick={() => setToggleLinks(!toggleLinks)}
+            >
+              <FaBars />
+            </button>
+          </div>
+          <div
+            className={`${
+              toggleLinks ? "links-container show-links" : "links-container"
+            }`}
+          >
+            <ul className="links ">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <ul className="social-icons">
+            {social.map((item) => {
+              const { id, url, icon } = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </main>
+  );
+};
+
+export default Navbar; */
+
+//INFO: auto-adjusting container for the height of the links using useRef hook
+/* const Navbar = () => {
+  const [toggleLinks, setToggleLinks] = useState(false);
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  window.onresize = function () {
+    if (window.innerWidth > 800) {
+      linksContainerRef.current.style.height = "auto";
+      setToggleLinks(false);
+    } else if (window.innerWidth <= 800) {
+      linksContainerRef.current.style.height = "0px";
+    }
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (toggleLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [toggleLinks]);
+
+  return (
+    <main>
+      <nav>
+        <div className="header-container">
+          <div className="nav-header">
+            <img src={logo} alt="logo" />
+            <button
+              className="nav-toggle"
+              onClick={() => setToggleLinks(!toggleLinks)}
+            >
               <FaBars />
             </button>
           </div>
@@ -57,4 +177,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Navbar; */
