58a29f0e3868d79bcdb438406ac578a454558653
sparrowCodes <deepaksu.047@gmail.com>
2024-07-13 16:41:08 +0530

    cart first push

---
+++ 14-cart/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CartContainer from "./CartContainer";
+import Navbar from "./Navbar.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <CartContainer />
+    </>
+  );
+};
+export default App;
---
+++ 14-cart/src/CartContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CartContainer = () => {
+  return (
+    <>
+      <main>CartContainer</main>;
+    </>
+  );
+};
+export default CartContainer;
